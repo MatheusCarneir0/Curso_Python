69f759d5293944ed1f0a080954561cd24b2c16ec
Matheus <matheusthayna082@gmail.com>
2025-12-24 15:45:14 -0300

    Aula 02 POO - Métodos de Classes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from pessoa import Pessoa           
 
+# p1 = Pessoa('Matheus', 2000)
+# p2 = Pessoa('Maria', 2005)
+
+
 p1 = Pessoa('Matheus', 2000)
-p2 = Pessoa('Maria', 2005)
-
-
-print(f'{p1.nome} tem {p1.get_ano_nascimento()}')
-print(p2.get_ano_nascimento())
+print(p1)
+print(p1.nome, p1.idade)
+p1.get_ano_nascimento()
